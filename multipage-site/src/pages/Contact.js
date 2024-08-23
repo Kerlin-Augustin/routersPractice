4f191a2d4a42d8fa1f9c551af0bc90d7e570c6b7
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-08-23 15:49:14 -0400

    routes

---
+++ multipage-site/src/pages/Contact.js
@@ -0,0 +1,8 @@
+export default function Contacts(){
+  return (
+    <div>
+      <h2>Contacts</h2>
+      <p>lorem ipsum</p>
+    </div>
+  )
+}
---
+++ multipage-site/src/pages/Home.js
@@ -0,0 +1,8 @@
+export default function Hone() {
+  return (
+    <div>
+      <h2>Homepage</h2>
+      <p>lorem</p>
+    </div>
+  )
+}
