4f39d96853948b1c798e53ab5d98edcc243eaf96
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-08-24 13:31:03 -0400

    params

--- multipage-site/src/pages/Contact.js
+++ multipage-site/src/pages/Contact.js
@@ -1,8 +1,16 @@
+import { useLocation } from 'react-router-dom';
+
 export default function Contacts(){
+
+  const querySelector = useLocation().search
+  const queryParams = new URLSearchParams(querySelector)
+  const name = queryParams.get('name')
+
   return (
     <div>
       <h2>Contacts</h2>
       <p>lorem ipsum</p>
+      <p>{name}</p>
     </div>
   )
 }
